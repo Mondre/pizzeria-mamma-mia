38396aa1e7e840ad7cab327031b1da9ccc988d8d
Mondre <claudioflash99.cm@gmail.com>
2025-12-17 10:38:47 +0100

    Fix: Add firebase_options.dart and update Android SDK/NDK

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,8 +7,8 @@ plugins {
 
 android {
     namespace = "com.mammamia.pizzeria_mamma_mia"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    compileSdk = 35
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
